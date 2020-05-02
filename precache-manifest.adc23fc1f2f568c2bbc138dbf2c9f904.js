72c56a3eb72a34af99a3c43f99dc8fb668b7c557
jhm304 <jhm304@gmail.com>
2020-05-02 11:43:20 -0700

    Updates

---
+++ precache-manifest.adc23fc1f2f568c2bbc138dbf2c9f904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f887b650d75d1e0452f98dc827c1ed6",
+    "url": "/https//jhm304.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c7d77581fc469e88659f",
+    "url": "/https//jhm304.github.io/monsters-rolodex/static/css/main.a2ca696a.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/https//jhm304.github.io/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//jhm304.github.io/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d77581fc469e88659f",
+    "url": "/https//jhm304.github.io/monsters-rolodex/static/js/main.2c24bfb0.chunk.js"
+  },
+  {
+    "revision": "24349d631115e49d72d2",
+    "url": "/https//jhm304.github.io/monsters-rolodex/static/js/runtime-main.f831a639.js"
+  }
+]);
